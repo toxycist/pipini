dcd03f7b9bfc255796f8f5dbdd12477e563225af
ATR <che22uncle@gmail.com>
2025-08-24 21:51:17 +0300

    React and Express are communicating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ data, setData ] = useState(null)
+  
+  useEffect(() => {
+    const sendApiCall = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/')
+        setData(response.data)
+        console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    sendApiCall()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{data}</div>
     </>
   )
 }
